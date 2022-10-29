7583cd7d4ee352320cb8e2318bf18e9380b24c46
Admin <aydin.ibrahimov.a@asoiu.edu.az>
2022-10-29 13:02:11 +0400

    commit

---
+++ src/main/java/com/aydinibrhmv/socialapp/modul/User.java
@@ -0,0 +1,20 @@
+package com.aydinibrhmv.socialapp.modul;
+
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "user")
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    private LocalDate birthDate;
+    private String firstname;
+    private String lastname;
+}
